871b2bf8d91ad9166b8cf60da54cf7fa883d2cd3
blakereeves19 <blake.reeves19@gmail.com>
2019-01-21 12:00:33 -0600

    Completed lab

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = (props) => {
-    return (
-        <h1>Hello World!</h1>
-    )
+// const App = (props) => {
+//     return (
+//         <h1>Hello World!</h1>
+//     )
+// }
+
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            text: "React is awesome!",
+            hasLoaded: false
+        };
+    }
+
+    handleInputChange(value) {
+        this.setState({ text: value });
+    }
+
+    handleToggle() {
+        this.setState({ hasLoaded: true });
+    }
+
+    componentDidMount() {
+        this.setState({ hasLoaded: true });
+    }
+
+    render() {
+
+        if (this.state.hasLoaded === true) {
+            let title = 'React';
+            return (
+                <React.Fragment>
+                    <h1>Hello {title}! {this.state.text}</h1>
+                    <input
+                        value={this.state.text}
+                        placeholder={this.state.text}
+                        onChange={(event) => this.handleInputChange(event.target.value)}
+                    />
+                    <button
+                        onClick={(event) => this.handleToggle(event.target)}
+                    >Click to Load!</button>
+                </React.Fragment>
+            );
+        } else {
+            return (
+                <React.Fragment>
+                    <h1>Loading...</h1>
+                    <button
+                        onClick={(event) => this.handleToggle(event.target)}
+                    >Click to Load!</button>
+                </React.Fragment>
+            );
+        }
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(
-    <App />,
+    <App text="React is awesome"/>,
     document.getElementById('root')
 );
